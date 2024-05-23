09aa9f03682f4e1e69d0dfe2cb141fa03d57a041
Mauri Berisvil <95480927+MBerisvil@users.noreply.github.com>
2024-05-23 16:29:38 -0300

    Python Game Snake

---
+++ Python Game Snake/snake.py
@@ -0,0 +1,91 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+import random
+
+# pygame setup
+pygame.init()
+square_width = 800
+pixel_width = 50
+screen = pygame.display.set_mode([square_width] * 2)
+clock = pygame.time.Clock()
+running = True
+message1 = "Welcome to the snake game"
+message2 = "Game Over"
+
+def generate_starting_position():
+    position_range = (pixel_width // 2, square_width - pixel_width // 2, pixel_width)
+    return random.randrange(*position_range), random.randrange(*position_range)
+
+
+
+def reset():
+    target.center = generate_starting_position()
+    snake_pixel.center = generate_starting_position()
+    return [snake_pixel.copy()]
+
+def isOutOfBounds():
+    if snake_pixel.bottom > square_width or snake_pixel.top < 0 or snake_pixel.right > square_width or snake_pixel.left < 0:
+        return True
+    return False
+
+
+# Create a snake
+snake_pixel = pygame.rect.Rect([0, 0, pixel_width - 2, pixel_width - 2])
+snake_pixel.center = generate_starting_position()
+snake = [snake_pixel.copy()]
+snake_direction = (0, 0)
+snake_lenght = 1
+
+# Create a target
+target = pygame.rect.Rect((0, 0, pixel_width, pixel_width))
+target.center = generate_starting_position()
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+
+    # RENDER YOUR GAME HERE
+
+    if isOutOfBounds():
+       snake_lenght = 1
+       snake = reset()
+
+    if snake_pixel.center == target.center:
+        target.center = generate_starting_position()
+        snake_lenght += 1
+        snake.append(snake_pixel.copy())
+
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        snake_direction = (0, -pixel_width)
+    if keys[pygame.K_s]:
+        snake_direction = (0, pixel_width)
+    if keys[pygame.K_a]:
+        snake_direction = (-pixel_width, 0)
+    if keys[pygame.K_d]:
+        snake_direction = (pixel_width, 0)
+
+
+    for snake_part in snake:
+        pygame.draw.rect(screen, "green", snake_part)
+   
+    pygame.draw.rect(screen, "red", target)
+
+    snake_pixel.move_ip(snake_direction)
+    snake.append(snake_pixel.copy())
+    snake = snake[1:]
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(10)  # limits FPS to 60
+
+pygame.quit()
